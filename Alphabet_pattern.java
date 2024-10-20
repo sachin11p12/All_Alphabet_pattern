ef09e5e103e220c27d0262f9ae11183f650f4ece
Sachin _Tiwari <167885715+sachin11p12@users.noreply.github.com>
2024-10-20 09:59:02 +0530

    Update Alphabet_pattern.java

--- Alphabet_pattern.java
+++ Alphabet_pattern.java
@@ -182,6 +182,21 @@ public class Alphabet_pattern {
             }
             System.out.println();
         }
+/* 
+*
+*
+*
+*
+* * * * * 
 
+*/
+        for (int i = 1; i <=5 ; i++) {
+            for (int j = 1; j <=5 ; j++) {
+                if (i==5 || j==1)
+                    System.out.print("* ");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
